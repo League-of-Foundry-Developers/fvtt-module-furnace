7ca9e23d960aecd24d05752c27313b812c52639f
Cody Robibero <cody@robibe.ro>
2020-04-04 07:43:23 -0600

    Fix throwing parse error in Edge

--- DrawingTools/Drawing.js
+++ DrawingTools/Drawing.js
@@ -158,7 +158,7 @@ class FurnaceDrawing extends Drawing {
           this.bg = this.addChild(new PIXI.Container());
           this.bg.tile = this.bg.addChild(sprite)
         }
-      } catch {
+      } catch (_){
         this.texture = null;
         this.bg = null;
       }
