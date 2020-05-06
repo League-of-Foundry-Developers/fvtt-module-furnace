a187d15e343696274e033543ebdc3c5acfc5dc29
Youness Alaoui <kakaroto@kakaroto.homelinux.net>
2020-05-06 17:47:14 -0400

    Add a 'idx' hb helper to access a specific array index

--- QoL/HBHelpers.js
+++ QoL/HBHelpers.js
@@ -4,5 +4,6 @@ Handlebars.registerHelper({
     now: () => new Date(),
     roll: (formula) => new Roll(formula).roll().total,
     multiply: (value1, value2) => Number(value1) * Number(value2),
-    divide: (value1, value2) => Number(value1) / Number(value2)
+    divide: (value1, value2) => Number(value1) / Number(value2),
+    idx: (array, idx) => array && array.length > idx ? array[idx] : ""
 });
