8c41a3b028dcb30e733e79e4f6aa9727bc4ea8db
Youness Alaoui <kakaroto@kakaroto.homelinux.net>
2020-07-01 00:04:48 -0400

    Add handling of HB helpers conflicting with variable names (obsidian)

--- QoL/HBHelpers.js
+++ QoL/HBHelpers.js
@@ -2,8 +2,14 @@
 H.registerHelpers(Handlebars)
 Handlebars.registerHelper({
     now: () => new Date(),
-    roll: (formula) => new Roll(formula).roll().total,
+    roll: function (formula) {
+        if (formula.name === "roll") return this.roll;
+        return new Roll(formula).roll().total
+    },
     multiply: (value1, value2) => Number(value1) * Number(value2),
     divide: (value1, value2) => Number(value1) / Number(value2),
-    idx: (array, idx) => array && array.length > idx ? array[idx] : ""
+    idx: function (array, idx) {
+      if (array.name === "idx") return this.idx;
+      return array && array.length > idx ? array[idx] : ""
+    }
 });
