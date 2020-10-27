c7f41d3fd48d58c5abe802c30bd57a831b6b72ff
Youness Alaoui <kakaroto@kakaroto.homelinux.net>
2020-10-26 21:56:54 -0400

    Do not patch hook method unless debug is enabled

--- QoL/Debug.js
+++ QoL/Debug.js
@@ -2,7 +2,6 @@ CONFIG.debug.furnace = true;
 
 class FurnaceDebug {
     static init() {
-            
         game.settings.register("furnace", "enableDebug", {
             name: game.i18n.localize("FURNACE.SETTINGS.enableDebug"),
             hint: game.i18n.localize("FURNACE.SETTINGS.enableDebugHint"),
@@ -12,34 +11,40 @@ class FurnaceDebug {
             type: Boolean,
             onChange: value => {
                 CONFIG.debug.furnace = value
+                this.maybePatchHooks();
             }
         });
         CONFIG.debug.furnace = game.settings.get("furnace", "enableDebug");
+        this.maybePatchHooks();
     }
     static log(...args) {
         if (CONFIG.debug.furnace)
             console.log("Furnace : ", ...args);
     }
+    static maybePatchHooks() {
+        if (!CONFIG.debug.furnace) return;
+        if (this._patchApplied) return;
+        this._patchApplied = true;
+        // Replace functions here so we can debug the call to 'init'
+        FurnacePatching.replaceFunction(Hooks, "callAll", function (hook, ...args) {
+            if (CONFIG.debug.furnace) {
+                const args_comma = []
+                args.forEach(a => { args_comma.push(a); args_comma.push(",") })
+                args_comma.pop();
+                console.log("DEBUG | Calling All Hooks : " + hook + "(", ...args_comma, ")")
+            }
+            return FurnacePatching.callOriginalFunction(this, "callAll", hook, ...args)
+        });
+        FurnacePatching.replaceFunction(Hooks, "call", function (hook, ...args) {
+            if (CONFIG.debug.furnace) {
+                const args_comma = []
+                args.forEach(a => { args_comma.push(a); args_comma.push(",") })
+                args_comma.pop();
+                console.log("DEBUG | Calling Hook : " + hook + "(", ...args_comma, ")")
+            }
+            return FurnacePatching.callOriginalFunction(this, "call", hook, ...args)
+        });
+    }
 }
 
-// Replace functions here so we can debug the call to 'init'
-FurnacePatching.replaceFunction(Hooks, "callAll", function (hook, ...args) {
-    if (CONFIG.debug.furnace) {
-        args_comma = []
-        args.forEach(a => { args_comma.push(a); args_comma.push(",") })
-        args_comma.pop();
-        console.log("DEBUG | Calling All Hooks : " + hook + "(", ...args_comma, ")")
-    }
-    return FurnacePatching.callOriginalFunction(this, "callAll", hook, ...args)
-});
-FurnacePatching.replaceFunction(Hooks, "call", function (hook, ...args) {
-    if (CONFIG.debug.furnace) {
-        args_comma = []
-        args.forEach(a => { args_comma.push(a); args_comma.push(",") })
-        args_comma.pop();
-        console.log("DEBUG | Calling Hook : " + hook + "(", ...args_comma, ")")
-    }
-    return FurnacePatching.callOriginalFunction(this, "call", hook, ...args)
-});
-
-Hooks.on('init', FurnaceDebug.init)
+Hooks.on('init', () => FurnaceDebug.init())
